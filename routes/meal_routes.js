10a30b189156901008a2f46b6a3d4a2e34437c37
Joey <joeyburgt@hotmail,com>
2018-05-15 14:37:14 +0200

    Revert "Revert "Routes verbeterd""
    This reverts commit ecd4455ecc12c952111bbe28c0869a2f9a0685f8.

--- routes/meal_routes.js
+++ routes/meal_routes.js
@@ -3,20 +3,10 @@ let express = require('express');
 let router = express.Router();
 let maaltijd_controllers = require('../controllers/maaltijd_controllers');
 
-//maak een nieuwe maaltijd aan
 router.post('/api/studentenhuis/:huisId/maaltijd', maaltijd_controllers.createMaaltijd);
-
-//verkrijg een maaltijd
 router.get('/api/studentenhuis/:huisId/maaltijd', maaltijd_controllers.getMaaltijd);
-
-//verkrijg een maaltijd aan de hand van zijn id
 router.get('/api/studentenhuis/:huisId/maaltijd/:maaltijdId', maaltijd_controllers.getMaaltijdById);
-
-//voer een maaltijd in
 router.put('/api/studentenhuis/:huisId/maaltijd/:maaltijdId', maaltijd_controllers.putMaaltijd);
-
-//Verwijder een maaltijd
 router.delete('/api/studentenhuis/:huisId/maaltijd/:maaltijdId', maaltijd_controllers.deleteMaaltijd);
-
-//export router
+//wegschrijven
 module.exports = router;
