7931595e30e0cad18fb5e13bd5a7162fe2a15c3d
Joey <joeyburgt@hotmail,com>
2018-05-15 15:31:02 +0200

    Om te pullen...

--- routes/meal_routes.js
+++ routes/meal_routes.js
@@ -5,11 +5,6 @@ let maaltijd_controllers = require('../controllers/maaltijd_controllers');
 
 router.post('/api/studentenhuis/:huisId/maaltijd', maaltijd_controllers.createMaaltijd);
 router.get('/api/studentenhuis/:huisId/maaltijd', maaltijd_controllers.getMaaltijd);
-<<<<<<< HEAD
-
-
-=======
->>>>>>> a33b2c75c7cc8666b227a11605a59fa68326016f
 router.get('/api/studentenhuis/:huisId/maaltijd/:maaltijdId', maaltijd_controllers.getMaaltijdById);
 router.put('/api/studentenhuis/:huisId/maaltijd/:maaltijdId', maaltijd_controllers.putMaaltijd);
 router.delete('/api/studentenhuis/:huisId/maaltijd/:maaltijdId', maaltijd_controllers.deleteMaaltijd);
