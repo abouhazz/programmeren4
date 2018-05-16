36539a079bb7a13f6556afe15b3d94d9eeddb280
Joey <joeyburgt@hotmail,com>
2018-05-16 14:29:49 +0200

    auth controller fixes

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -13,7 +13,7 @@ module.exports = {
 // token mag niet empty zijn
 
 
-        if(token.includes("")) {
+        if (token.includes("")) {
 
 
             const json = {
@@ -25,71 +25,126 @@ module.exports = {
         }
 
 
-
         //token ontmantelen
         auth.decodeToken(token, (error, payload) => {
-            if(error)      {
+            if (error) {
                 //afgekeurd
                 const error = new ApiError(error.message || error, 401);
                 next(error);
 
-            } else    {
+            } else {
                 //goedgekeurd
                 console.log("Token was approved with payload: ");
                 console.dir(payload);
                 request.user = payload.sub;
                 next();
-              }
-          })
+            }
+        })
     },
 
     //login initaliseren
     login: function (request, response) {
         const email = request.body.email;
         const password = request.body.password;
-        console.log("Login contains: " + email + ", "+ password);
+        console.log("Login contains: " + email + ", " + password);
         const query = {
             sql: "SELECT ID FROM user WHERE email = " + email + "AND password = " + password,
-            timeout : 2000
+            timeout: 2000
         };
         // error afhandelen
         db.query(query, function (error, rows) {
             if (error) {
-                console.log("Error! Please try again!")
+                console.log("Error!");
+            }
+            //match met db
+            else if (rows[0]) {
+                const ID = rows.insertId;
+                const token = auth.encodeToken(ID, email);
+                const json = {
+                    "token": token,
+                    "email": email
+                }
+                response.status(200).json(json);
+            }
+            //geen match
+            else {
+                const json = {
+                    "message": "No match found! Please register!",
+                    "code": "401",
+                    "datetime": moment()
+                }
+                response.status(401).json(json);
             }
         })
-
-
-
     },
 
     //register initaliseren
     register: function (request, response) {
 
         //body gegevens ophalen
-        const firstname = request.body.firstname || "";
-        const lastname = request.body.lastname || "";
-        const email = request.body.email || "";
-        const password = request.body.password || "";
-
+        const firstname = request.body.firstname;
+        const lastname = request.body.lastname;
+        const email = request.body.email;
+        const password = request.body.password;
         //hier laten zien dat een veld niet leeggelaten mag worden
         if (!([firstname, lastname, email, password].includes(''))) {
 
-
-            const token = auth.encodeToken(firstname, email);
             const json = {
-                "token": token,
-                "email": email
+                "message": "Invalid token!",
+                "code": 401,
+                "datetime": moment
             };
-            response.status(200).json(json);
+            response.status(401).json(json);
         }
+// checks uitvoeren op user en email
+        var query = {
+            sql: "SELECT * FROM user WHERE email = " + email,
+            timeout: 2000
+        }
+        if (error) {
+            console.log("Error!")
+            response.status(401).json(error);
+        }
+        if (rows[0]) {
+            console.log("Email is already registered!");
+            response.status(401).json(error);
+        }
+        //nieuwe user aanmaken!
+        else {
+            var query2 = {
+                sql: "INSERT INTO user (Voornaam, Achternaam, Email, Password)" +
+                "VALUES " + firstname + lastname + email + password + ");",
+                timeout: 2000
+            }
+            db.query(query, function (error, rows) {
+                if (error) {
+                    console.log("Error!")
+                } else {
+                    console.log("Added user to db");
+                }
+            })
+            var query3 = {
+                sql: "SELECT ID FROM user WHERE email" + email,
+                timeout: 2000
+            }
+            db.query(query, function (error, rows) {
+                if (error) {
+                    console.log("Error!");
+                }
+                //nu token maken
+                else {
+                    //variabele maken
+                    var ID = rows.insertId;
+                    var token = auth.encodeToken(ID, email);
+                    var json = {
+                        "token": token,
+                        "email": email
+                    }
+                    response.status(200).json(json);
+                }
+            })
+        }
+    }
+}
 
 
-
-
-
-
-
-
-    },
-};
